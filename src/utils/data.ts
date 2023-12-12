89cc8243371a886e2bbc2d53d5a3c3d17cc5e15f
Derek <dereklau15@gmail.com>
2023-12-12 11:39:11 +0900

    Gather assets for players, teams, countries, compile data

---
+++ src/utils/data.ts
@@ -0,0 +1,134 @@
+import Aguerd from "../assets/players/Aguerd.png"
+import Haaland from "../assets/players/Haaland.png"
+import Hodgson from "../assets/players/Hodgson.png"
+import Luiz from "../assets/players/Luiz.png"
+import Mitoma from "../assets/players/Mitoma.png"
+import Odegaard from "../assets/players/Ødegaard.png"
+import Palhinha from "../assets/players/Palhinha.png"
+import Pickford from "../assets/players/Pickford.png"
+import Salah from "../assets/players/Salah.png"
+import Shaw from "../assets/players/Shaw.png"
+import Silva from "../assets/players/Silva.png"
+import Trippier from "../assets/players/Trippier.png"
+
+import Arsenal from "../assets/teams/Arsenal.png"
+import Brighton from "../assets/teams/Brighton.png"
+import City from "../assets/teams/City.png"
+import Chelsea from "../assets/teams/Chelsea.png"
+import Everton from "../assets/teams/Everton.png"
+import Fulham from "../assets/teams/Fulham.png"
+import Liverpool from "../assets/teams/Liverpool.png"
+import Newcastle from "../assets/teams/Newcastle.png"
+import Palace from "../assets/teams/Palace.png"
+import United from "../assets/teams/United.png"
+import Villa from "../assets/teams/Villa.png"
+import WestHam from "../assets/teams/West-Ham.png"
+
+import Brazil from "../assets/nationality/Brazil.png"
+import Egypt from "../assets/nationality/Egypt.png"
+import England from "../assets/nationality/England.png"
+import Japan from "../assets/nationality/Japan.png"
+import Morocco from "../assets/nationality/Morocco.png"
+import Norway from "../assets/nationality/Norway.png"
+import Portugal from "../assets/nationality/Portugal.png"
+
+const players = [
+  {
+    id: 1,
+    first: "Erling",
+    last: "Haaland",
+    image: Haaland,
+    team: City,
+    nationality: Norway,
+  },
+  {
+    id: 2,
+    first: "Kaoru",
+    last: "Mitoma",
+    image: Mitoma,
+    team: Brighton,
+    nationality: Japan,
+  },
+  {
+    id: 3,
+    first: "Mohamed",
+    last: "Salah",
+    image: Salah,
+    team: Liverpool,
+    nationality: Egypt,
+  },
+  {
+    id: 4,
+    first: "Martin",
+    last: "Ødegaard",
+    image: Odegaard,
+    team: Arsenal,
+    nationality: Norway,
+  },
+  {
+    id: 5,
+    first: "Douglas",
+    last: "Luiz",
+    image: Luiz,
+    team: Villa,
+    nationality: Brazil,
+  },
+  {
+    id: 6,
+    first: "João",
+    last: "Palhinha",
+    image: Palhinha,
+    team: Fulham,
+    nationality: Portugal,
+  },
+  {
+    id: 7,
+    first: "Luke",
+    last: "Shaw",
+    image: Shaw,
+    team: United,
+    nationality: England,
+  },
+  {
+    id: 8,
+    first: "Nayef",
+    last: "Aguerd",
+    image: Aguerd,
+    team: WestHam,
+    nationality: Morocco,
+  },
+  {
+    id: 9,
+    first: "Thiago",
+    last: "Silva",
+    image: Silva,
+    team: Chelsea,
+    nationality: Brazil,
+  },
+  {
+    id: 10,
+    first: "Kieran",
+    last: "Trippier",
+    image: Trippier,
+    team: Newcastle,
+    nationality: England,
+  },
+  {
+    id: 11,
+    first: "Jordan",
+    last: "Pickford",
+    image: Pickford,
+    team: Everton,
+    nationality: England,
+  },
+  {
+    id: 12,
+    first: "Roy",
+    last: "Hodgson",
+    image: Hodgson,
+    team: Palace,
+    nationality: England,
+  },
+]
+
+export default players
